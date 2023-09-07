296b3b2395e00b48388e553e1ad3d0f8ccefee82
meheedihasaan <mehedihasan111941@gmail.com>
2023-09-07 21:23:08 +0600

    Modify urls

--- starter-commons/src/main/java/com/disl/dbfile/constants/AppConstants.java
+++ starter-commons/src/main/java/com/disl/dbfile/constants/AppConstants.java
@@ -4,13 +4,13 @@ public final class AppConstants {
 
     private AppConstants() {}
 
-    public static final String FILE_UPLOAD_URL = "https://files.dbl.dhrubok.me/api/file-handler/upload";
+    public static final String FILE_UPLOAD_URL = "";
 
-    public static final String FILE_DOWNLOAD_URL = "https://files.dbl.dhrubok.me/api/file-handler/load-file";
+    public static final String FILE_DOWNLOAD_URL = "";
 
-    public static final String FILE_INFO_URL = "https://files.dbl.dhrubok.me/api/file-handler/id/";
+    public static final String FILE_INFO_URL = "";
 
-    public static final String FILE_DELETE_URL = "https://files.dbl.dhrubok.me/api/file-handler/delete/";
+    public static final String FILE_DELETE_URL = "";
 
     public static final String FILE = "file";
 
